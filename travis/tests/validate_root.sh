b5ba5d350b581d61f6d8e93838e07dd1425e1ced
Luis Rueda <lurueda@cisco.com>
2017-10-27 16:13:59 -0500

    test travis

--- travis/tests/validate_root.sh
+++ travis/tests/validate_root.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
-# Bash3 Boilerplate. Copyright (c) 2014, kvz.io
 
 set -o errexit
 set -o pipefail
 set -o nounset
-# Uncomment the next line if you want to debug
 # set -o xtrace
 
 # Set magic variables for current file & dir
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 __base="$(basename ${__file} .sh)"
+__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
+LOGGER=echo
+LOGGER_EMERGENCY="echo $(date +"%Y-%m-%d %H:%M") | EMERG | "
+LOGGER_ALERT="echo $(date +"%Y-%m-%d %H:%M") | ALERT | "
+LOGGER_CRITICAL="echo $(date +"%Y-%m-%d %H:%M") | CRIT | "
+LOGGER_ERROR="echo $(date +"%Y-%m-%d %H:%M") | ERROR | "
+LOGGER_WARNING="echo $(date +"%Y-%m-%d %H:%M") | WARN | "
+LOGGER_NOTICE="echo $(date +"%Y-%m-%d %H:%M") | NOTICE | "
+LOGGER_INFO="echo $(date +"%Y-%m-%d %H:%M") | INFO | "
+LOGGER_DEBUG="echo $(date +"%Y-%m-%d %H:%M") | DEBUG | "
+
+arg1="${1:-}"
 
 # Validate README.md exists
 if [ -e ${__dir}/../../README.md ]
@@ -20,4 +30,11 @@ else
    echo "README.md does not exists, failing"
    exit 1
 fi
+
+yaml_files=$(find . -name "*yml")
+for yml_file in ${yaml_files}
+do
+  ${__dir}/validate_root.py validate yml ${yml_file}
+done
+
 #ls -1a ${__dir}/../../ | grep -v "^.$" | grep -v "^..$"
